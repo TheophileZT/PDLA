8319b47e35eb394a5ec247c45f3a146cc7047813
z6TM <theozt25@gmail.com>
2023-10-17 14:34:28 +0200

    2nd table created

--- createtable.sql
+++ createtable.sql
@@ -6,3 +6,15 @@ CREATE TABLE IF NOT EXISTS USER(
     Email VARCHAR(45) NOT NULL UNIQUE,
     UserType ENUM('Helper','Validator','Needer')
 );
+
+CREATE TABLE IF NOT EXISTS MISSIONS(
+    IdMission INT PRIMARY KEY AUTO_INCREMENT,
+    IdNeeder INT NOT NULL,
+    IdHelper INT NOT NULL,
+    Title VARCHAR(45) NOT NULL,
+    Description VARCHAR(500) NOT NULL,
+    Date DATETIME NOT NULL,
+    Status ENUM('Pending','Accepted','Refused','Done') NOT NULL,
+    FOREIGN KEY (IdNeeder) REFERENCES USER(IdUser),
+    FOREIGN KEY (IdHelper) REFERENCES USER(IdUser)
+);
