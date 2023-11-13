11d5f5b53f95fe92772f92494ccb69e9e75b365d
sebaici <kelian.sbc@gmail.com>
2023-11-13 11:28:38 +0100

    modif de la vue sign up et des noms d'arguments

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,10 +2,10 @@ package controller;
 
 public class Controller {
 
-    public static void logIn(String text, char[] password) {
+    public static void logIn(String login, char[] password) {
 
     }
-    public static void createNewUser(String text, String text1, String text2, String password, boolean selected, boolean selected1, boolean selected2) {
+    public static void createNewUser(String firstName, String lastName, String email, String password, boolean isNeeder, boolean isVolunteer, boolean isValidator) {
 
     }
 }
