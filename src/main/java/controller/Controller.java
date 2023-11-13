a1d4d610c39df4a56bf26ff79fdbfed0004d99c2
TzT_laptop <theozt25@gmail.com>
2023-11-13 11:34:54 +0100

    ii

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,10 +2,13 @@ package controller;
 
 public class Controller {
 
+    Connection connect = Controller.getConnection(srv-bdens.insa-toulouse.fr, projet_gei_011, shu6AeNg);
+    Statement statement = connect.createStatement();
+    String query = "SELECT * FROM User";
     public static void logIn(String text, char[] password) {
 
     }
-    public static void createNewUser(String text, String text1, String text2, String password, boolean selected, boolean selected1, boolean selected2) {
+    public static void createNewUser(String firstName, String lastName, String email, String password, boolean isNeeder, boolean isVolunteer, boolean isValidator) {
 
     }
 }
