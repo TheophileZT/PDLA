8b5f58413184544cbd762d6aa8015b87485e2c78
Kelian S <kelian.sbc@gmail.com>
2023-12-09 19:57:24 +0100

    Update view login et start

--- src/main/java/view/LogInView.java
+++ src/main/java/view/LogInView.java
@@ -3,66 +3,118 @@ package view;
 import controller.Controller;
 
 import javax.swing.*;
+import java.awt.*;
 import java.sql.SQLException;
 import java.util.Arrays;
 
 public class LogInView {
 
     public static void create() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("LogInFrame");
-        frame.setSize(900, 600);
+        // Create and set up the window.
+        JFrame frame = new JFrame("Login");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setLayout(new BorderLayout());
+        frame.getContentPane().setBackground(new Color(200, 200, 200)); // Couleur de fond
+
+        // Panel for form components
         JPanel formPanel = new JPanel();
-        JLabel labelTitle = new JLabel("Log In", JLabel.CENTER);
-        JLabel labelEmail = new JLabel("Email");
-        JTextField textFieldEmail = new JTextField();
-        JLabel labelPassword = new JLabel("Password");
-        JPasswordField passwordField = new JPasswordField();
-        JButton validateButton = new JButton("Validate");
+        formPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
 
-        //Set the components properties.
-        labelTitle.setFont(labelTitle.getFont().deriveFont(20.0f));
-        formPanel.setLayout(new BoxLayout(formPanel, BoxLayout.Y_AXIS));
-        frame.getContentPane().add(formPanel, "North");
-        frame.getContentPane().add(validateButton, "South");
-        labelEmail.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 10));
-        labelPassword.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 10));
+        // Title
+        JLabel labelTitle = new JLabel("Login", JLabel.CENTER);
+        labelTitle.setFont(new Font("Arial", Font.BOLD, 24));
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        formPanel.add(labelTitle, gbc);
 
-        //Add the components to the panel.
-        formPanel.add(labelTitle);
-        formPanel.add(labelEmail);
-        formPanel.add(textFieldEmail);
-        formPanel.add(labelPassword);
-        formPanel.add(passwordField);
+        // Email
+        JLabel labelEmail = new JLabel("Email:");
+        JTextField textFieldEmail = new JTextField(20);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        formPanel.add(labelEmail, gbc);
+        gbc.gridx = 1;
+        formPanel.add(textFieldEmail, gbc);
 
-        //Event listener
-        validateButton.addActionListener(actionEvent -> {
-                    String email = textFieldEmail.getText();
-                    String password = Arrays.toString(passwordField.getPassword());
-                    if (email.isEmpty() || password.isEmpty()) {
-                        JOptionPane.showMessageDialog(frame, "Please fill all the fields");
-                    } else {
-                        String[] ret;
-                        try {
-                            ret = Controller.logIn(email, password);
-                        } catch (SQLException e) {
-                            throw new RuntimeException(e);
-                        }
-                        if (ret[0].equals("Login successful")) {
-                            frame.dispose();
-                            switch (ret[1]) {
-                                case "Needer" -> ViewNeeder.create(Integer.parseInt(ret[2]));
-                                case "Helper" -> ViewHelper.create();
-                                case "Validator" -> ViewValidator.create();
-                            }
-                        } else {
-                            JOptionPane.showMessageDialog(frame, ret[0]);
-                        }
-                    }
-                });
+        // Password
+        JLabel labelPassword = new JLabel("Password:");
+        JPasswordField passwordField = new JPasswordField(20);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        formPanel.add(labelPassword, gbc);
+        gbc.gridx = 1;
+        formPanel.add(passwordField, gbc);
 
-        //Display the window.
+        // Login Button
+        JButton loginButton = new JButton("Login");
+        loginButton.setBackground(new Color(46, 139, 87)); // SeaGreen
+        loginButton.setForeground(Color.WHITE);
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 1;
+        gbc.anchor = GridBagConstraints.CENTER;
+        formPanel.add(loginButton, gbc);
+
+        // Sign Up Button
+        JButton signUpButton = new JButton("Sign Up");
+        signUpButton.setBackground(new Color(255, 69, 0)); // Red-Orange
+        signUpButton.setForeground(Color.WHITE);
+        gbc.gridx = 1;
+        gbc.anchor = GridBagConstraints.CENTER;
+        formPanel.add(signUpButton, gbc);
+
+        // Event listeners
+        loginButton.addActionListener(e -> {
+            actionLogin(textFieldEmail, passwordField, frame);
+        });
+
+        signUpButton.addActionListener(e -> {
+            frame.dispose();
+            SignUpView.create();
+        });
+
+        passwordField.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                if (evt.getKeyCode() == java.awt.event.KeyEvent.VK_ENTER) {
+                    actionLogin(textFieldEmail, passwordField, frame);
+                }
+            }
+        });
+
+        // Add components to the frame
+        frame.add(formPanel, BorderLayout.CENTER);
+
+        // Display the window.
         frame.setVisible(true);
+    }
+
+    private static void actionLogin(JTextField textFieldEmail, JPasswordField passwordField, JFrame frame) {
+        String email = textFieldEmail.getText();
+        String password = Arrays.toString(passwordField.getPassword());
+        if (email.isEmpty() || password.isEmpty()) {
+            JOptionPane.showMessageDialog(frame, "Please fill in both fields");
+        } else {
+            String[] ret;
+            try {
+                ret = Controller.logIn(email, password);
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            if (ret[0].equals("Login successful")) {
+                frame.dispose();
+                switch (ret[1]) {
+                    case "Needer" -> ViewNeeder.create(Integer.parseInt(ret[2]));
+                    case "Helper" -> ViewHelper.create();
+                    case "Validator" -> ViewValidator.create();
+                }
+            } else {
+                JOptionPane.showMessageDialog(frame, ret[0]);
+            }
+        }
     }
 }
--- src/main/java/view/ViewStart.java
+++ src/main/java/view/ViewStart.java
@@ -1,38 +1,58 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
+
 public class ViewStart {
 
     public static void create() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("LogInFrame");
-        frame.setSize(900, 600);
+        // Create and set up the window.
+        JFrame frame = new JFrame("Login");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panelButtons = new JPanel();
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // Mettre en plein écran
+        frame.setLayout(new BorderLayout());
+
+        // Panel for buttons
+        JPanel panelButtons = new JPanel(new FlowLayout());
         JLabel labelText = new JLabel("Welcome to \"name of the app\"!");
         labelText.setHorizontalAlignment(JLabel.CENTER);
-        labelText.setFont(labelText.getFont().deriveFont(15.0f));
-        labelText.setBorder(BorderFactory.createEmptyBorder(20, 10, 0, 10));
+        labelText.setFont(labelText.getFont().deriveFont(20.0f)); // Ajuster la taille de la police
+        labelText.setBorder(BorderFactory.createEmptyBorder(50, 10, 0, 10)); // Ajuster les marges
         JButton buttonLogIn = new JButton("Log In");
         JButton buttonSignUp = new JButton("Sign Up");
+
+        // Ajuster la taille des boutons
+        Dimension buttonSize = new Dimension(150, 40);
+        buttonLogIn.setPreferredSize(buttonSize);
+        buttonSignUp.setPreferredSize(buttonSize);
+
+        // Add buttons to the panel
         panelButtons.add(buttonLogIn);
         panelButtons.add(buttonSignUp);
-        panelButtons.setBorder(BorderFactory.createEmptyBorder(30, 10, 10, 10));
-        frame.getContentPane().add(labelText, "North");
-        frame.getContentPane().add(panelButtons);
-        //Display the window.
+        panelButtons.setBorder(BorderFactory.createEmptyBorder(100, 10, 10, 10)); // Ajuster les marges
+
+        // Add components to the frame
+        frame.getContentPane().add(labelText, BorderLayout.NORTH);
+        frame.getContentPane().add(panelButtons, BorderLayout.CENTER);
+
+        // Style the buttons to match the provided style
+        buttonLogIn.setBackground(new Color(46, 139, 87)); // SeaGreen
+        buttonLogIn.setForeground(Color.WHITE);
+        buttonSignUp.setBackground(new Color(255, 69, 0)); // Red-Orange
+        buttonSignUp.setForeground(Color.WHITE);
+
+        // Display the window.
         frame.setVisible(true);
 
-        //ouverture du formulaire d'inscription
-        buttonSignUp.addActionListener(actionEvent -> {
+        // Action listeners
+        buttonSignUp.addActionListener(e -> {
             SignUpView.create();
             frame.dispose();
         });
 
-        buttonLogIn.addActionListener(actionEvent -> {
+        buttonLogIn.addActionListener(e -> {
             LogInView.create();
             frame.dispose();
         });
     }
-
 }
